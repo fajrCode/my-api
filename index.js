b17eabff7791b78cbf8c421c59f6b7da0c23426e
fajrCode <fajri42vandi@gmail.com>
2025-01-12 22:23:39 +0700

    init project

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Hello World'
+    });
+});
+
+app.listen(5000, () => {
+    console.log('Server is listening on port 5000');
+});
