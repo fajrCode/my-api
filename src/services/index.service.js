97d0c0239279d6e19e3cca0af017a179ebb635e1
Ridwan <ridwannmuhammad159@gmail.com>
2025-01-19 21:11:18 +0700

    chore: init prisma

--- src/services/index.service.js
+++ src/services/index.service.js
@@ -1,9 +1,10 @@
+import prisma from '../configs/database.js';
+
 export const getData = () => {
+    const admins = prisma.admin.findMany();
     const data = {
         message: 'Hello World',
-        user:{
-            nama: 'Fulan'
-        }
+        user: admins,
     };
 
     return data;
