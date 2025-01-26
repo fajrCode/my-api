a28b8a29b9f1b81090d176d1cb65930e3a5287e3
Ridwan <ridwannmuhammad159@gmail.com>
2025-01-26 22:04:31 +0700

    feat: auth | complete register

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import express from 'express';
 import router from './routes/index.route.js';
+import authRouter from './routes/auth.route.js';
+import { errorHandler, resourceNotFound } from './middlewares/errorHandler.js';
 
 const app = express();
 
+app.use(express.json());
+
 app.use(router);
+app.use(authRouter);
+
+app.use(resourceNotFound);
+app.use(errorHandler);
 
 app.listen(5000, () => {
     console.log('Server is listening on port 5000');
---
+++ src/services/auth.service.js
@@ -0,0 +1,23 @@
+import prisma from '../configs/database.js';
+import { error400 } from '../utils/customError.js';
+import { hash } from 'bcrypt';
+
+export const registerHandler = async (data) => {
+    const admin = await prisma.admin.findUnique({
+        where: {
+            email: data.email
+        }
+    })
+
+    if (admin) throw error400('Email already exists');
+
+    const hashedPassword = await hash(data.password, 10);
+
+    return await prisma.admin.create({
+        data: {
+            name: data.name,
+            email: data.email,
+            password: hashedPassword
+        }
+    });
+};
