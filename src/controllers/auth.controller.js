f5d3b78cca348cb01043d7c1ff56763454ae4162
fajri <fajri42vandi@gmail.com>
2025-02-02 20:41:03 +0700

    feat: auth | api login

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import { res200, res201 } from '../utils/customResponse.js';
-import { registerValidator } from '../validations/auth.validator.js';
+import { loginValidator, registerValidator } from '../validations/auth.validator.js';
 import { error400 } from '../utils/customError.js';
-import { registerHandler } from '../services/auth.service.js';
+import { registerHandler, loginHandler } from '../services/auth.service.js';
 
 export const register = async (req, res, next) => {
     try {
@@ -13,6 +13,21 @@ export const register = async (req, res, next) => {
         await registerHandler(value)
 
         res201(res, 'User registered successfully');
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const login = async (req, res, next) => {
+    try {
+        const { error, value } = loginValidator.validate(req.body);
+        if (error) {
+            throw error400(error.details[0].message);
+        }
+
+        const data = await loginHandler(value);
+
+        res200(res, 'User logged in successfully', data);
     } catch (error) {
         next(error);
     }
