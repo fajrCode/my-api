34f4421717bb58741fde6757e6700920a5e6b390
fajri <fajri42vandi@gmail.com>
2025-02-09 21:07:44 +0700

    feat: user management | all endpoint

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import router from './routes/index.route.js';
 import authRouter from './routes/auth.route.js';
+import userRouter from './routes/user.route.js';
 import { errorHandler, resourceNotFound } from './middlewares/errorHandler.js';
 
 const app = express();
@@ -9,6 +10,7 @@ app.use(express.json());
 
 app.use(router);
 app.use(authRouter);
+app.use(userRouter);
 
 app.use(resourceNotFound);
 app.use(errorHandler);
--- src/utils/customError.js
+++ src/utils/customError.js
@@ -9,3 +9,9 @@ export const error401 = (message) => {
     error.statusCode = 401;
     return error
 };
+
+export const error404 = (message) => {
+    const error = new Error(message);
+    error.statusCode = 404;
+    return error
+};
